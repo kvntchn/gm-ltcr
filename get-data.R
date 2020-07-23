574f20dddfd33694d56623915901f8b26ce9ab54
Kevin Chen <kevchen@berkeley.edu>
2020-07-22 17:10:51 -0700

    Using sl3 implementation

--- get-data.R
+++ get-data.R
@@ -80,9 +80,19 @@ if (!"cohort_analytic" %in% ls()) {
 
 length(table(cohort_analytic$studyno)); sum(cohort_analytic$y)
 
-# Make categorical variables for ease ####
+# Make categorical variables ####
 # probs <- seq(0, 1, 0.25)
-probs <- seq(0, 1, 0.1)
+# probs <- seq(0, 1, 1/3)
+categorize.cols <- c(
+	"employment.years", "sincehire.years", "year", "age",
+	"assembly", "machining", "off", "cumulative_off",
+	"yin16", "cum_soluble", "cum_straight", "cum_synthetic")
+probs <- sapply(categorize.cols, function(x) {
+	if (x == "year") {seq(0, 1, 1/10)} else if (
+		x %in% paste0("cum_", c("soluble", "straight", "synthetic"))) {
+		c(0, 2/3, 3/4, 1)
+	} else {seq(0, 1, 1/4)}
+}, simplify = F)
 
 if ("Age" %in% names(cohort_analytic)) {
 	cohort_analytic <- cohort_analytic[,-"Age", with = F]}
@@ -98,35 +108,59 @@ if ("Time_spent_machining" %in% names(cohort_analytic)) {
 		"Cumulative_soluble_exposure",
 		"Cumulative_straight_exposure",
 		"Cumulative_synthetic_exposure",
-		"Employment_status"
+		"Employment_status",
+		"Duration_of_employment"
 	), with = F]
 }
 
 cohort_analytic[,`:=`(
 	age = age.year2/365,
+	Age.exp = exp(age.year2/365),
 	Employment_status = factor(ifelse(year <= year(yout), "At work", "Left work"),
 														 levels = c("At work", "Left work")),
-	Plant = factor(plant, levels = as.character(1:3)),
+	Plant = factor(plant, 1:3, paste("Plant", 1:3)),
 	Race = factor(race, levels = c("White", "Not white"), labels = c("White", "Black")),
-	Sex = factor(sex, levels = c("M", "F"), labels = c("Male", "Female"))
+	Sex = factor(sex, levels = c("M", "F"), labels = c("Male", "Female")),
+	employment.years = time_length(difftime(
+		apply(data.frame(
+			as.Date(paste0(year , "-12-31")),
+			yoc,
+			yod,
+			yout
+		), 1, min, na.rm = T),
+		yin,
+	), 'year')
 )]
 
-covariate.quantile <- apply(
-	cohort_analytic[, .(
-		sincehire.years, year, age,
-		assembly, machining, off, cumulative_off,
-		yin16, cum_soluble, cum_straight, cum_synthetic
-	)], 2, function(x) {
-		quantile.tmp <- quantile(x[cohort_analytic[, y == 1]], probs)
+# cohort_analytic[year > year(yout), .(year, Duration_of_employment, yin, yout), by = .(studyno)]
+
+covariate.quantile <- sapply(categorize.cols, function(categorize.col = categorize.cols[1]) {
+		x <- cohort_analytic[, categorize.col, with = F]
+		quantile.tmp <- quantile(unlist(x[cohort_analytic$y == 1]), unlist(probs[categorize.col]))
 		if (quantile.tmp[1] == quantile.tmp[2] &
-				quantile.tmp[1] == 0) {quantile.tmp[1] <- -Inf} else {
+				quantile.tmp[1] == 0) {
+			quantile.tmp[1] <- -Inf} else {
 					quantile.tmp[1] <- min(x)
-				}
+			}
+		if (categorize.col %in% c("cumulative_off", "cum_soluble", "cum_straight", "cum_synthetic")) {
+			quantile.tmp <- c(-Inf, 0, quantile.tmp)
+		}
 		quantile.tmp[length(quantile.tmp)] <- max(x)
-		return(quantile.tmp)
-	})
+		return(sort(quantile.tmp))
+	}, simplify = F)
+
+# Human-specified cutpoints
+# Keeping 1st and last, but changing middle cutpoints
+change.middle <- function(var.name, ...) {
+	sort(c(..., covariate.quantile[[var.name]][c(1, length(covariate.quantile[[var.name]]))]))
+}
+covariate.quantile$age <- change.middle('age', 55, 70)
+covariate.quantile$yin16  <- change.middle("yin16", 1945, 1960)
+covariate.quantile$employment.years <- change.middle("employment.years", 10, 20)
+covariate.quantile$cumulative_off <- change.middle("cumulative_off", 0, 1)
 
 cohort_analytic[,(c(
+	"Duration_of_employment",
 	"Years_since_hire",
 	"Calendar_year",
 	"Age",
@@ -139,8 +173,8 @@ cohort_analytic[,(c(
 	"Cumulative_straight_exposure",
 	"Cumulative_synthetic_exposure"
 )):=lapply(
-	colnames(covariate.quantile), function(x) {
-		x <- cut(get(x), unique(covariate.quantile[,x]), include.lowest = T, dig.lab = 4)
+	names(covariate.quantile), function(x = names(covariate.quantile)[1]) {
+		x <- cut(get(x), unique(covariate.quantile[[x]]), include.lowest = T, dig.lab = 4)
 		levels(x) <- gsub("\\[-Inf,0\\]", "0", levels(x))
 		x
 	})]
--- survival-sl.R
+++ survival-sl.R
@@ -6,6 +6,9 @@
 # Preliminaries
 library(here)
 library(SuperLearner)
+# sl3, the Coyle, Nejazi, Malencia, Sofrygin implementation
+# remotes::install_github("tlverse/sl3")
+library(sl3)
 # # XGBoost
 # install.packages("drat", repos="https://cran.rstudio.com")
 # drat:::addRepo("dmlc")
@@ -18,30 +21,35 @@ source(here::here('get-data.R'))
 setorder(cohort_analytic, studyno, year)
 
 # Drop unnecessary variables and take subset for fast testing
-X_cat.names <- c("Years_since_hire", "Calendar_year", "Age",
-	"Race", "Plant", "Sex", "Year_of_hire",
-	"Time_spent_machining", "Time_spent_off", "Cumulative_time_off",
+# Categorically-coded variables only
+X.names <- c(
+	"Duration_of_employment",
+	"Calendar_year", "Age.exp",
+	"Race", "Plant", "Sex",
+	"Year_of_hire",
+	"Cumulative_time_off",
 	"Cumulative_soluble_exposure",
 	"Cumulative_straight_exposure",
 	"Cumulative_synthetic_exposure",
 	"Employment_status")
-X.names <- c(
-	"sincehire.years", "year", "age",
+# Continuous and categorical variables
+X_continuous.names <- c(
+	"employment.years", "year", "age",
+	"Race", "Plant", "Sex",
 	"yin16",
-	"machining", "off", "cumulative_off",
+	"cumulative_off",
 	"cum_soluble",
 	"cum_straight",
-	"cum_synthetic"
+	"cum_synthetic",
+	"Employment_status"
 )
-col.names <- c(
-	"studyno", "year",
-	"age.year1", "age.year2", "year1", "year2",
-	"y",
-	X_cat.names, X.names
-)
+col.names <- unique(c(
+	"studyno", "year", "y", "Age",
+	X.names, X_continuous.names
+))
 
 set.seed(236)
-studyno.sample <- sample(unique(cohort_analytic$studyno), 1500)
+studyno.sample <- sample(unique(cohort_analytic$studyno), 2000)
 cohort_select <- cohort_analytic[
 	# studyno %in% studyno.sample
 	, col.names, with = F]
@@ -68,49 +76,118 @@ str(cohort_select)
 # 
 # cohort_select <- cbind(cohort_select[,!col.is_long_factor, with = F], one_hot.bind(one_hot))
 
-# Fit ####
+# sl3 pipeline ####
+
+# Make sure predict.gam returns "response"
+Lrnr_gam$private_methods$.predict <- function (task) {
+	predictions <- stats::predict(private$.fit_object, newdata = task$X, type = "response")
+	predictions <- as.numeric(predictions)
+	return(predictions)
+}
+
+# Instantiate task
+task <- make_sl3_Task(as.data.frame(cohort_select),
+											covariates = X.names,
+											outcome = "y",
+											outcome_type = "binomial",
+											id = "studyno")
+
+# Make learners
 sl.library <- c("sl", "mean",
-								"lm", "glm", # categorical
-								"glm.interaction", # continuous
-								"step", "step.interaction", # categorical
-								"glmnet", "biglasso", # categorical
-								# "ridge", # Not possible for binomial
-								"gam", # continuous
-								# "loess", # Not possible for binomial
-								"earth", # continuous
-								"ranger", # categorical
-								"knn", # continuous, else too many ties
-								"ksvm", # continous, else vector memory exausted
-								# "svm", # Infeasible
-								"rpart", "rpartPrune", # categorical
-								# "xgboost" # categorical # very long computation time
+								"glm",
+								# "glm.interaction",
+								# "step",
+								# "step.interaction",
+								"glmnet",
+								"gam",
+								"ranger",
+								# "knn", # breaks the cv
+								# "rpart", # breaks the cv,
+								"xgboost",
+								NULL
 								)
-source(here::here("sl_library.R"))
-# Set up parallel computation - no windows
-num_cores <- RhpcBLASctl::get_num_cores()
-options(mc.cores = num_cores - 1)
-set.seed(236, "L'Ecuyer-CMRG")
-sl <- CV.SuperLearner(
-	Y = cohort_select$y,
-	X = as.data.frame(cohort_select[,c(X_cat.names, X.names), with = F]),
-	family = "binomial",
-	SL.library = paste0("SL.", sl.library[-1], "2"),
-	cvControl = list(V = 10),
-	parallel = "multicore"
+
+# Instantiate
+sapply(sl.library[-1], function(x = "rpart") {
+	assign(paste0("lrnr_", x),
+				 {if (sum(grepl(x, sl3_list_learners("binomial"))) > 0) {
+				 	make_learner(get(paste0("Lrnr_", x)))
+				 } else {
+				 	# Use SuperLearner package, if needed
+				 		make_learner(Lrnr_pkg_SuperLearner, paste0("SL.", x))}
+				 	},
+				 envir = .GlobalEnv)
+})
+
+# Run and check system time
+sapply(sl.library[-1], function(x) {
+	system.time(
+	assign(paste0("lrnr_", x, "_fit"),
+				 get(paste0("lrnr_", x))$train(task),
+				 envir = .GlobalEnv),
+	F)
+})
+
+# verify that the learner is fit
+sapply(sl.library[-1], function(x) {
+				 get(paste0("lrnr_", x, "_fit"))$is_trained
+})
+
+# No screening for now, just stacking
+stack <- make_learner(Stack,
+											sapply(sl.library[-1], function(x) {
+												get(paste0("lrnr_", x))
+												}))
+
+stack_fit <- stack$train(task)
+
+# Cross-validation
+cv_stack <- Lrnr_cv$new(stack)
+cv_fit <- cv_stack$train(task)
+
+# Super Learner
+metalearner <- make_learner(Lrnr_nnls)
+cv_task <- cv_fit$chain(task)
+ml_fit <- metalearner$train(cv_task)
+
+sl_pipeline <- make_learner(Pipeline, stack_fit, ml_fit)
+sl_preds <- sl_pipeline$predict()
+
+# Save sl3 results ####
+saveRDS(sl_preds, file = to_drive_D(here::here("resources", "sl_preds.rdata")))
+
+sl.predict <- data.table(
+	sl = sl_preds,
+	sapply(sl.library[-1], function(x) {
+				 get(paste0("lrnr_", x, "_fit"))$predict()
+})
 	)
 
-saveRDS(sl, file = to_drive_D(here::here("resources", "sl.rdata")))
+# # Basic implementation ####
+# # Set up parallel computation - no windows
+# num_cores <- RhpcBLASctl::get_num_cores()
+# options(mc.cores = num_cores - 1)
+# set.seed(236, "L'Ecuyer-CMRG")
+# sl <- SuperLearner(
+# 	Y = cohort_select$y,
+# 	X = as.data.frame(cohort_select[,c(X.names), with = F]),
+# 	family = "binomial",
+# 	SL.library = paste0("SL.", sl.library[-1][-3]),
+# 	cvControl = list(V = 10)
+# 	# parallel = "multicore"
+# 	)
+# # Save SuperLearner results ####
+# saveRDS(sl, file = to_drive_D(here::here("resources", "sl.rdata")))
 # sl <- readRDS(file = to_drive_D(here::here("resources", "sl.rdata")))
-
-# Get predictions
-sl.predict <- as.data.table(
-	cbind(studyno = cohort_select$studyno,
-	year = cohort_select$year,
-	sl$library.predict)
-	)
-
-names(sl.predict) <- gsub("2_All$|^SL.", "", names(sl.predict))
-
+# 
+# # Get predictions
+# sl.predict <- as.data.table(
+# 	cbind(studyno = cohort_select$studyno,
+# 	year = cohort_select$year,
+# 	sl$library.predict)
+# 	)
+# names(sl.predict) <- gsub("2_All$|^SL.", "", names(sl.predict))
+# 
 # box_save(sl.predict,
 # 				 dir_id = 117568282871,
 # 				 file_name = paste0("sl.predict.rdata"),
@@ -119,10 +196,10 @@ names(sl.predict) <- gsub("2_All$|^SL.", "", names(sl.predict))
 # Probability of being alive ####
 # (1 - Pr(subject died at t = 1)) * (1 - Pr(subject died at t = 2)) * ... * (1 - Pr(subject died at t = t))
 cohort_select[, (paste0(sl.library, ".fitted")):=(
-	lapply(sl.predict[,-(1:2)], function(x) {x})
+	lapply(sl.predict, function(x) {x})
 )]
 cohort_select[, (paste0(sl.library, ".prob")):=(
-	lapply(sl.predict[,-(1:2)], function(x) {
+	lapply(sl.predict, function(x) {
 		cumprod(1 - x)})
 )]
 cohort_select[,`:=`(
@@ -131,7 +208,7 @@ cohort_select[,`:=`(
 	mortality = as.numeric(max(y) > 0)),
 	by = .(studyno)]
 prob.tab <- cohort_select[I == N, c(
-	"studyno",
+	"studyno", "year",
 	paste0(sl.library, ".prob"),
 	"mortality",
 	"Age", "Year_of_hire", "Race", "Sex",
@@ -144,12 +221,15 @@ prob.tab <- cohort_select[I == N, c(
 names(prob.tab) <- gsub(" exposure$", "", gsub("_", " ", names(prob.tab)))
 
 # Probabilities by stratum ####
-for (covariate in c("Age", "Employment status", "Race", "Sex", "Year of hire")) {
+for (covariate in c("Age", "Employment status", "Race", "Sex", "Year of hire", paste("Cumulative", c("straight", "soluble", "synthetic")))) {
 	tmp.tab <- prob.tab[mortality == 0, .(
 		`sl` = mean(sl.prob),
 		`mean` = mean(mean.prob),
 		`glm` = mean(glm.prob),
-		`ranger` = mean(ranger.prob)
+		# `glmnet` = mean(glmnet.prob),
+		`gam` = mean(gam.prob),
+		`ranger` = mean(ranger.prob),
+		`xgboost` = mean(xgboost.prob)
 	), by = .(get(covariate))][order(get),]
 	names(tmp.tab)[1] <- covariate
 	# box_save(
@@ -164,6 +244,7 @@ for (covariate in c("Age", "Employment status", "Race", "Sex", "Year of hire")) 
 	)
 	assign(paste0(gsub(" ", "_" , tolower(covariate)), ".prob.tab"),
 				 tmp.tab, envir = .GlobalEnv)
+	print(tmp.tab)
 }
 
 # ROC ####
@@ -186,9 +267,12 @@ roc.ggtab <- rbindlist(lapply(sl.library, function (method) {
 setDT(roc.ggtab)
 
 roc.ggtab[,`:=`(AUC = factor(AUC, sort(as.numeric(unique(AUC)), T)))]
-roc.ggtab[,`:=`(method = factor(
-	method, roc.ggtab[,.(method = method[1]), by = .(AUC)][order(AUC),]$method
-	))]
+pair <- roc.ggtab[,.(AUC = AUC[1]), by = .(method)][order(AUC),]
+roc.ggtab[,`:=`(
+	method = factor(method, pair$method, paste0(pair$method, " (AUC = ", pair$AUC, ")"))
+	# AUC = factor(
+	# 	AUC, sort(unique(AUC)))
+	)]
 
 box_save(
 	roc.ggtab,
@@ -204,7 +288,7 @@ box_write(
 	description = "ROC results by method"
 )
 
-roc.threshold <- roc.ggtab[,.(
+roc.threshold <- roc.ggtab[, .(
 	threshold.min = min(threshold[is.finite(threshold)]),
 	threshold.Q2 = quantile(threshold[is.finite(threshold)], 0.25),
 	threshold.med = median(threshold[is.finite(threshold)]),
@@ -226,16 +310,20 @@ roc.ggtab[I == 1 | method == "mean"] %>% ggplot(aes(
 	x = Specificity, y = Sensitivity, color = method
 )) + geom_step(size = 0.5) +
 	geom_segment(x = -1, y = 0, xend = 0, yend = 1, color = "black", linetype = 2, size = 0.5) +
-	geom_rect(aes(xmin = 0.5, xmax = 0.5, ymin = 0.5, ymax = 0.5,
-								fill = AUC), alpha = 0) +
-	guides(fill = guide_legend(
-		override.aes = list(alpha = 1))) +
+	# geom_rect(aes(xmin = 0.5, xmax = 0.5, ymin = 0.5, ymax = 0.5, fill = AUC), alpha = 0) +
+	# guides(fill = guide_legend(
+	# 	override.aes = list(alpha = 1))) +
 	scale_x_reverse() + mytheme -> roc.ggplot
 
 roc.ggplot
 
 sapply(sl.library, function(method) {
 	get(paste0(method, ".roc"))$auc})
+
+sapply(X.names, function(x) {
+	if (x == "Age.exp") {x <- "Age"}
+	table(cohort_select[, x, with = F])
+}, simplify = F)
 
 # # Compile plot
 # library(tikzDevice)
